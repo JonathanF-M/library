374fc14d3447f5e74f78948739595ddc9ce55e67
Jonathan F <jfariasm647@gmail.com>
2021-06-25 18:51:39 -0700

    formatted page and displayed placeholder books

---
+++ script.js
@@ -0,0 +1,36 @@
+let myLibrary = [];
+
+function Book(title, author, pages, read) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+    this.info = function() {
+        return `${this.title} by ${this.author}, ${this.pages} pages, ${ !this.read ? "not" : ""} read yet.` };
+}
+
+function addBookToLibrary(book) {
+    myLibrary.push(book);
+}
+
+const theHobbit = new Book("The Hobbit", "JRR Tolkien", 249, 0);
+addBookToLibrary(theHobbit);
+
+for(let i = 0; i <= 6; i++){
+    const book = new Book(`Book${i}`, `Author${i}`, 420, 69)
+    addBookToLibrary(book);
+}
+
+container = document.querySelector("#container");
+
+function displayBooks(){
+    for(let i=0; i < myLibrary.length; i++){
+        bookCard = document.createElement("div");
+        bookCard.setAttribute('class', 'bookCard');
+        bookCard.setAttribute('data-index', `${i}`)
+        bookCard.textContent = myLibrary[i].info();
+        container.appendChild(bookCard);
+    }
+}
+
+displayBooks();
